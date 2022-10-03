70b5b4dc9f641e77ab6c8a142df72c0733d3cfaf
Rasmus3liasson <105283065+Rasmus3liasson@users.noreply.github.com>
2022-10-03 17:00:56 +0200

    dsdsdsdgfgfg

---
+++ script.js
@@ -0,0 +1,14 @@
+const btnP = document.querySelector(".btnP");
+const aboutMe = document.querySelector("#aboutMe");
+const contact = document.querySelector("#contact");
+const instagram = document.querySelector("#instagram");
+const twitter = document.querySelector("#twitter");
+const linkedIn = document.querySelector("#linkedin");
+const mail = document.querySelector("#mail");
+
+btnP.addEventListener("click");
+instagram.addEventListener("click");
+twitter.addEventListener("click");
+linkedIn.addEventListener("click");
+mail.addEventListener("click");
+aboutMe.addEventListener("click");
