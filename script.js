91dbd5498fbffabb50503de43c092250cd4feed1
Rasmus3liasson <105283065+Rasmus3liasson@users.noreply.github.com>
2022-10-04 19:52:17 +0200

    hfdjkhfdf

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const btnP = document.querySelector(".btnP");
-const aboutMe = document.querySelector("#aboutMe");
-const contact = document.querySelector("#contact");
 const instagram = document.querySelector("#instagram");
 const twitter = document.querySelector("#twitter");
 const linkedIn = document.querySelector("#linkedin");
@@ -11,4 +9,3 @@ instagram.addEventListener("click");
 twitter.addEventListener("click");
 linkedIn.addEventListener("click");
 mail.addEventListener("click");
-aboutMe.addEventListener("click");
