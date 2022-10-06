77939597e56c0748b29c82a18484739f2fa2ace8
Rasmus3liasson <105283065+Rasmus3liasson@users.noreply.github.com>
2022-10-06 12:06:51 +0200

    uyuyu

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const instagram = document.querySelector("#instagram");
 const twitter = document.querySelector("#twitter");
 const linkedIn = document.querySelector("#linkedin");
 const mail = document.querySelector("#mail");
+const aboutMe = document.querySelector("#aboutMe");
 
 btnP.addEventListener("click");
 instagram.addEventListener("click");
